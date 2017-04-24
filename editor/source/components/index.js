014ab314f5102418d95fa3c0450d658334ebc991
imeoer <imeoer@gmail.com>
2017-04-24 20:47:27 +0800

    remove editor project from source

--- editor/source/components/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { immutableRenderDecorator } from 'react-immutable-render-mixin'
-
-class Component extends React.Component {}
-
-immutableRenderDecorator(Component)
-
-export default Component
--- editor/source/store/history.js
+++
@@ -1,2 +0,0 @@
-import { browserHistory } from 'react-router'
-export default browserHistory
